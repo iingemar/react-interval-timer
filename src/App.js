843f84e89a4c6f513daa16ddc83d04a309c2076e
Ingemar <iingemar@gmail.com>
2020-05-26 10:04:08 +0200

    Switched App to React.component

--- src/App.js
+++ src/App.js
@@ -8,36 +8,38 @@ import Button from 'react-bootstrap/Button';
 import './App.css';
 import Timer from './Timer';
 
-const App = () => (
-    <Container>
-        <Jumbotron>
-            <h1 className="header">Interval timers! :D</h1>
-        </Jumbotron>
-        <Row>
-            <Col>
-                <Button variant="primary" size="lg">Start</Button>{' '}
-                <Button variant="secondary" size="lg">Stop</Button>{' '}
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <Alert variant="info">
-                    <Row>
-                        <Col><h1>Spring!</h1></Col>
-                        <Col><Timer /></Col>
-                    </Row>
-                </Alert>
-            </Col>
-        </Row>
-        <Row>
-            <Col><h2>Vila</h2></Col>
-            <Col><Timer /></Col>
-        </Row>
-        <Row>
-            <Col><h2>Spring</h2></Col>
-            <Col><Timer /></Col>
-        </Row>
-    </Container>
-);
-
-export default App;
+export default class extends React.Component {
+    render() {
+        return (
+            <Container>
+                <Jumbotron>
+                    <h1 className="header">Interval timers! :D</h1>
+                </Jumbotron>
+                <Row>
+                    <Col>
+                        <Button variant="primary" size="lg">Start</Button>{' '}
+                        <Button variant="secondary" size="lg">Stop</Button>{' '}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Alert variant="info">
+                            <Row>
+                                <Col><h1>Spring!</h1></Col>
+                                <Col><Timer /></Col>
+                            </Row>
+                        </Alert>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col><h2>Vila</h2></Col>
+                    <Col><Timer /></Col>
+                </Row>
+                <Row>
+                    <Col><h2>Spring</h2></Col>
+                    <Col><Timer /></Col>
+                </Row>
+            </Container>
+        );
+    }
+}
