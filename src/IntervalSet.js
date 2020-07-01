cde0581680a6f35d860a7293808a1782f0ec0486
Ingemar <iingemar@gmail.com>
2020-07-01 08:41:04 +0200

    Added interval sets css

--- src/IntervalSet.js
+++ src/IntervalSet.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Alert from 'react-bootstrap/Alert';
-import './App.css';
-import Timer from './Timer';
+import './IntervalSet.css';
 import * as intervalType from './IntervalTypes';
 
 
@@ -14,9 +13,9 @@ export default class extends React.Component {
 
     getDescription() {
         if (this.props.set.type === intervalType.RUN) {
-            return 'HIGH';
+            return 'SPRIING!!';
         } else {
-            return 'LOW';
+            return 'Vila';
         }
     }
 
@@ -26,8 +25,8 @@ export default class extends React.Component {
                 <Col>
                     <Alert variant="info">
                         <Row>
-                            <Col><h1>{ this.getDescription() }</h1></Col>
-                            <Col><Timer seconds={this.props.set.seconds }/></Col>
+                            <Col><span className="IntervalSet-description">{ this.getDescription() }</span></Col>
+                            <Col><span className="IntervalSet-seconds">{ this.props.set.seconds } sekunder</span></Col>
                         </Row>
                     </Alert>
                 </Col>
